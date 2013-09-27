edd90070fc29c64ae12e550ade82543baeba91b4
Amir H. Hajizamani <amir.h.hajizamani@gmail.com>
2013-09-27 01:00:15 +0100

    Count Changing algorithm with tests for degenerate cases

--- assignments/recfun/src/test/scala/recfun/CountChangeSuite.scala
+++ assignments/recfun/src/test/scala/recfun/CountChangeSuite.scala
@@ -8,19 +8,32 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class CountChangeSuite extends FunSuite {
   import Main.countChange
+
+  test("countChange: 0 money means only 1 way") {
+    assert(countChange(0, List(1, 2)) === 1)
+  }
+
+  test("countChange: 0 coins means only 0 ways") {
+    assert(countChange(4, List()) === 0)
+  }
+
+  test("countChange: negatice money means 0 ways") {
+    assert(countChange(-4, List(1, 2)) === 0)
+  }
+
   test("countChange: example given in instructions") {
-    assert(countChange(4,List(1,2)) === 3)
+    assert(countChange(4, List(1, 2)) === 3)
   }
 
   test("countChange: sorted CHF") {
-    assert(countChange(300,List(5,10,20,50,100,200,500)) === 1022)
+    assert(countChange(300, List(5, 10, 20, 50, 100, 200, 500)) === 1022)
   }
 
   test("countChange: no pennies") {
-    assert(countChange(301,List(5,10,20,50,100,200,500)) === 0)
+    assert(countChange(301, List(5, 10, 20, 50, 100, 200, 500)) === 0)
   }
 
   test("countChange: unsorted CHF") {
-    assert(countChange(300,List(500,5,50,100,20,200,10)) === 1022)
+    assert(countChange(300, List(500, 5, 50, 100, 20, 200, 10)) === 1022)
   }
 }
